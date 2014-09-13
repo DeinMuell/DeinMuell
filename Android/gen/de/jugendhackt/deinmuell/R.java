7cc407e53fa64f263b7bb12d46c85506f98f50fa
julisa99 <lisaihde@web.de>
2014-09-13 14:15:39 +0200

    Update

--- Android/gen/de/jugendhackt/deinmuell/R.java
+++ Android/gen/de/jugendhackt/deinmuell/R.java
@@ -36,6 +36,10 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int MeldeButton=0x7f050003;
+        public static final int Statistikbutton=0x7f050006;
+        public static final int SucheButton=0x7f050004;
+        public static final int TrashieButton=0x7f050005;
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
