784c31c073dbb901a488158cacb8f99732c9c096
julisa99 <lisaihde@web.de>
2014-09-13 16:07:37 +0200

    Menü fertig gemacht

--- Android/gen/de/jugendhackt/deinmuell/R.java
+++ Android/gen/de/jugendhackt/deinmuell/R.java
@@ -22,10 +22,20 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int background=0x7f020000;
+        public static final int button=0x7f020001;
+        public static final int buttonshape=0x7f020002;
+        public static final int buttonshape2=0x7f020003;
+        public static final int buttonshape3=0x7f020004;
+        public static final int buttonshape4=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int action_settings=0x7f080005;
+        public static final int angry_btn=0x7f080001;
+        public static final int angry_btn2=0x7f080002;
+        public static final int angry_btn3=0x7f080003;
+        public static final int angry_btn4=0x7f080004;
         public static final int container=0x7f080000;
     }
     public static final class layout {
--- Android/src/de/jugendhackt/deinmuell/MainActivity.java
+++ Android/src/de/jugendhackt/deinmuell/MainActivity.java
@@ -8,7 +8,10 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 import android.os.Build;
 
 
@@ -19,11 +22,16 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+        
+        Button btn_angry = (Button) findViewById(R.id.angry_btn);
+        btn_angry.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				Toast.makeText(MainActivity.this, "Müll melden", Toast.LENGTH_LONG).show();
+				
+			}
+		});
     }
 
 
@@ -46,19 +54,5 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
+   
 }
