4e7db059101143716590ac9854592af3187eadbe
julisa99 <lisaihde@web.de>
2014-09-14 10:12:47 +0200

    ....

--- Android/gen/de/jugendhackt/deinmuell/R.java
+++ Android/gen/de/jugendhackt/deinmuell/R.java
@@ -47,21 +47,22 @@ public final class R {
         public static final int trashie3=0x7f020013;
     }
     public static final class id {
-        public static final int action_info=0x7f09000e;
-        public static final int action_settings=0x7f09000f;
+        public static final int action_info=0x7f09000f;
+        public static final int action_settings=0x7f090010;
         public static final int angry_btn=0x7f090002;
         public static final int angry_btn2=0x7f090001;
         public static final int angry_btn3=0x7f090004;
         public static final int angry_btn4=0x7f090003;
-        public static final int button1=0x7f09000c;
+        public static final int button1=0x7f09000d;
         public static final int container=0x7f090000;
         public static final int editOrt=0x7f090005;
-        public static final int editOrtSuche=0x7f09000b;
+        public static final int editOrtSuche=0x7f09000c;
         public static final int editStraße=0x7f090006;
-        public static final int editStraßeSuche=0x7f09000a;
+        public static final int editStraßeSuche=0x7f09000b;
         public static final int evil_btn1=0x7f090007;
-        public static final int imageButton1=0x7f09000d;
-        public static final int spinner1=0x7f090009;
+        public static final int imageButton1=0x7f09000e;
+        public static final int spinner1=0x7f09000a;
+        public static final int textView1=0x7f090009;
         public static final int typ_spinner=0x7f090008;
     }
     public static final class layout {
